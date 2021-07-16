53603f781c05ebd307dca8ae12f1c4b44a3df26e
mahoney0101 <hoooch.mahoney@gmail.com>
2021-07-16 19:21:13 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using NodaTime.TimeZones;
+
+
+var TzdbIdsToWin = TzdbDateTimeZoneSource.Default.TzdbToWindowsIds;
+var WindowsIdToTzdbIds = TzdbDateTimeZoneSource.Default.WindowsToTzdbIds;
+
+string TzdbIdsToWinTxt = "IANA, Windows";
+string WindowsIdtoTzdbIdTxt = "Windows, IANA";
+
+TzdbIdsToWin
+    .ToList()
+    .ForEach(entry => {
+        TzdbIdsToWinTxt += String.Format("\n{0},{1}", entry.Key, entry.Value); 
+    });
+
+WindowsIdToTzdbIds
+    .ToList()
+    .ForEach(entry => {
+        WindowsIdtoTzdbIdTxt += String.Format("\n{0},{1}", entry.Key, entry.Value);
+    });
+
+System.IO.File.WriteAllText("IANAtoWinIds.csv", TzdbIdsToWinTxt);
+System.IO.File.WriteAllText("WindowsIdstoIANAs.csv", WindowsIdtoTzdbIdTxt);
+
+
